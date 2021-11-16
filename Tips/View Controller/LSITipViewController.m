f63bb193e07a6e2db7b83c2b019594250befe222
Bob Lauer <bob@lambdaschool.com>
2021-11-16 16:14:30 -0600

    BloomTech

--- Tips/View Controller/LSITipViewController.m
+++ Tips/View Controller/LSITipViewController.m
@@ -3,7 +3,7 @@
 //  Tips
 //
 //  Created by Spencer Curtis on 2/6/20.
-//  Copyright © 2020 Lambda School. All rights reserved.
+//  Copyright © 2020 BloomTech. All rights reserved.
 //
 
 #import "LSITipViewController.h"
